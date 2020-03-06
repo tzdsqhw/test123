cccda27db749ff4cf6d2dc3345ae3c6489387548
tzdsqw <taozhuodong@gmail.com>
2020-03-06 17:52:55 +0800

    1

--- src/csdn.py
+++ src/csdn.py
@@ -1,14 +1,22 @@
 # -*- coding:utf-8 -*-
 import requests
 from bs4 import BeautifulSoup
-class test():
-    r = requests.get("https://blog.csdn.net/m0_37857151/article/details/81330699")
-    demo = r.text
-    soup = BeautifulSoup(demo, "html.parser")
-    context = soup.find_all('div' , class_ = 'htmledit_views')
-    print(context)
 
-    context = soup.find_all('div' , class_ = 'htmledit_views')
-    for hh in context: print(hh.get_text())
+
+class csdn():
+    def __init__(self):
+        pass
+
+    def s(self):
+        r = requests.get("https://blog.csdn.net/m0_37857151/article/details/81330699")
+        demo = r.text
+        soup = BeautifulSoup(demo, "html.parser")
+        context = soup.find_all('div' , class_ = 'htmledit_views')
+        print(context)
+
+        context = soup.find_all('div' , class_ = 'htmledit_views')
+        for hh in context: print(hh.get_text())
+
+
 if __name__ == '__main__':
-    test
+    c = csdn()
